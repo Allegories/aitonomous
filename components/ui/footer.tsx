00fb3e6fd0caded88eed942a2d09390a0feb9d38
Allegories <aleegoris@gmail.com>
2025-02-14 17:48:31 +0700

    footer perusahaan

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -159,7 +159,7 @@ export default function Footer() {
             </div>
             <div className="text-sm">
               <p className="mb-3 text-indigo-200/65 text-center">
-               AI-tonomous © 2025 
+              © PT.AItonomous Inovasi Digital 
                 <span className="text-gray-700"> · </span>
                 <a
                   className="text-indigo-200/65 transition hover:text-indigo-500"
